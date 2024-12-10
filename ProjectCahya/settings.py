d73675cc9f1aa03bd512e034df35d84d3e588e88
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2024-12-10 06:16:20 +0200

    added header load-in animation

--- ProjectCahya/settings.py
+++ ProjectCahya/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-6^5*b++wv1sxlx_i92uqk12_bn&3m=s=kf^&7w3+)+=3o!dig=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
